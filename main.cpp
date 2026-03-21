20e3c865adeab8aaed6668708b3bb995685bef4a
sralnik99 <sralnik99@gmail.com>
2026-03-21 21:05:26 +0200

    xui

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+int main(int argc, char** argv) {
+    if(argv[1] == NULL) {
+        std::cout << "Enter a file please" << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    std::string str;
+    
+    char arr[30000];
+    std::memset(arr,0,sizeof(arr));
+
+    char c;
+    int i = 0;
+
+    while(file.get(c)) {
+        switch (c)
+        {
+        case '>':
+            i++;
+            break;
+        case '<':
+            i--;
+            break;
+        case '+':
+            arr[i]++;
+            break;
+        case '-':
+            arr[i]--;
+            break;
+        case '.':
+            std::putchar(arr[i]);
+            break;
+        case ',':
+            std::cerr << "Not implemented, sorry!" << std::endl;
+            break;
+
+        case '[':
+            std::cerr << "Not implemented, sorry!" << std::endl;
+            break;
+        case ']':
+            std::cerr << "Not implemented, sorry!" << std::endl;
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    return 0;
+}
